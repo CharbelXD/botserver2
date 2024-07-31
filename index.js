15a92a061d8863490c04ae4e2f2d024b52decba3
CharbelXD <120786321+CharbelXD@users.noreply.github.com>
2024-07-31 06:57:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,60 +7,55 @@ const app = express();
 const bodyParser = require("body-parser");
 const axios = require("axios");
 
-
 const port = process.env.PORT || 4040;
 const hook = process.env.WEBHOOK_URL;
-const{BOT_TOKEN, SERVER_URL} = process.env
+const { BOT_TOKEN, SERVER_URL } = process.env;
 
-const TELEGRAM_API=`https://api.telegram.org/bot${BOT_TOKEN}`;
+const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
 const URI = `/webhook/${BOT_TOKEN}`;
-const WEBHOOK_URL = SERVER_URL+URI
+const WEBHOOK_URL = SERVER_URL + URI;
 
 app.use(express.json());
 app.use(bodyParser.json());
 const init = async () => {
     const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
-    console.log(res.data)
-}
+    console.log(res.data);
+};
 
 app.listen(process.env.PORT || 5000, async () => {
-    console.log('app is running on port', process.env.PORT || 5000)
-    await init()
-})
+    console.log('app is running on port', process.env.PORT || 5000);
+    await init();
+});
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const web_link = "https://godzillaonton.netlify.app/";
 
 bot.start((ctx) => {
-  const startPayload = ctx.startPayload;
-  const urlSent = `${web_link}?ref=${startPayload}`;
-  // const urlSentTwo = `${web_linkTwo}?ref=${startPayload}`;
-  const user = ctx.message.from;
-  const userName = user.username ? `@${user.username}` : user.first_name;
-  ctx.replyWithMarkdown(`*Hey, ${userName}! Welcome to Godzilla Tap App!*
+    const startPayload = ctx.startPayload;
+    const urlSent = `${web_link}?ref=${startPayload}`;
+    // const urlSentTwo = `${web_linkTwo}?ref=${startPayload}`;
+    const user = ctx.message.from;
+    const userName = user.username ? `@${user.username}` : user.first_name;
+    ctx.replyWithMarkdown(`*Hey, ${userName}! Welcome to Godzilla Tap App!*
 
 Click on Play Now to start mining $GODZ Token`, {
-      reply_markup: {
-          inline_keyboard: [
-            [{ text: "⚡️Play now!⚡️", web_app: { url: urlSent } }] 
-            // [{ text: "Bot App Demo 2 🧩", web_app: { url: urlSentTwo } }],
-          
-          ],
-          in: true
-      },
-  });
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: "⚡️Play now!⚡️", web_app: { url: urlSent } }],
+                [{ text: "Join our Telegram group", url: "https://t.me/godzillaonton" }]
+                // [{ text: "Bot App Demo 2 🧩", web_app: { url: urlSentTwo } }],
+            ],
+            in: true
+        },
+    });
 });
 
+app.get("/", async (req, res) => {
+    res.send("Hello Get me here I work fine");
+});
 
-
-  
-
-  app.get("/", async (req, res) => {
-    res.send("Hello Get me here i work fineddd")
-  });
-
-  app.post(URI, (req, res) => {
+app.post(URI, (req, res) => {
     bot.handleUpdate(req.body);
     res.status(200).send('Received Telegram webhook');
 });
