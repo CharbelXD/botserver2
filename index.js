05e18006447bb7078d1f6493d11447f7c0d82292
CharbelXD <120786321+CharbelXD@users.noreply.github.com>
2024-09-04 06:22:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.listen(process.env.PORT || 5000, async () => {
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const web_link = "https://tdogtapapp.netlify.app/";
+const web_link = "https://marvicweb3.netlify.app/";
 
 bot.start((ctx) => {
     const startPayload = ctx.startPayload;
